897b21b9a38e00cc13c58909c076c16cf3258091
rockgem <63050183+rockgem@users.noreply.github.com>
2023-05-29 09:06:28 +0800

    panel anim

--- scenes/BuildingView.gd
+++ scenes/BuildingView.gd
@@ -1,6 +1,24 @@
 extends Control
 
 
+@onready var panel = $Panel
+
+
+
+func _ready():
+	panel.pivot_offset = panel.size / 2
+
+
 func _on_gui_input(event):
 	if event is InputEventScreenTouch and !event.pressed:
 		hide()
+
+
+func anim_panel():
+	panel.scale = Vector2.ZERO
+	var t = create_tween()
+	t.tween_property(panel, 'scale', Vector2(1, 1), .3).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
+
+
+func _on_visibility_changed():
+	anim_panel()
