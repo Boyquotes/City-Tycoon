ad713bec5bf8cdace6ea2339d69c8663d3577a69
rockgem <63050183+rockgem@users.noreply.github.com>
2023-05-30 21:25:20 +0800

    car path, layering for cars to index

---
+++ scenes/Paths.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+
+func _on_car_spawn_timer_timeout():
+	var c = get_children()
+	c.shuffle()
+	
+	var car = c[0]
+	car.get_node('PathFollow2D').progress_ratio = 0.0
+	
+	var t = create_tween()
+	t.tween_property(car.get_node('PathFollow2D'), 'progress_ratio', 1.0, 6.0)
