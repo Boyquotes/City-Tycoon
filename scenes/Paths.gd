4d4889623a3281b20afe6ce082771626ad003e81
rockgem <63050183+rockgem@users.noreply.github.com>
2023-05-31 08:46:22 +0800

    cars changes

--- scenes/Paths.gd
+++ scenes/Paths.gd
@@ -10,4 +10,4 @@ func _on_car_spawn_timer_timeout():
 	car.get_node('PathFollow2D').progress_ratio = 0.0
 	
 	var t = create_tween()
-	t.tween_property(car.get_node('PathFollow2D'), 'progress_ratio', 1.0, 6.0)
+	t.tween_property(car.get_node('PathFollow2D'), 'progress_ratio', 1.0, 13.0)
